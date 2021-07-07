cc9dd46acc5b905b06d162c890aef4eab77e6fce
Vadim-V-S <vaddimsav@gmail.com>
2021-07-07 15:00:50 +0500

    Добавьте файлы проекта.

---
+++ SF_12.1.5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace SF_12._1._5
+{
+    class Program
+    {
+
+        static User user = new User();
+
+        static void Main(string[] args)
+        {
+            user.PrintLogins();
+
+
+            if (!user.IsSuscribePremium(user.InputLogin()))
+            {
+                ShowAds();
+            }
+            user.Greetings();
+
+
+            Console.ReadKey();
+        }
+
+       
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу");
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ SF_12.1.5/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SF_12._1._5
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User[] user;
+
+        int index;
+
+        public User[] UsersList()
+        {
+            user = new[]
+            {
+                new User{Login = "andrew", Name ="Андрей", IsPremium = false},
+                new User{Login = "mike", Name= "Михаил", IsPremium = false},
+                new User{Login = "john", Name="Иван",IsPremium = true}
+            };
+
+            return user;
+        }
+
+        public void PrintLogins()
+        {
+            Console.WriteLine("* Зарегистрированные пользователи: ");
+            foreach (var log in UsersList())
+                Console.WriteLine("логин: {0}/ подписка: {1}", log.Login, log.IsPremium);
+
+        }
+
+
+        public string InputLogin()
+        {
+            Console.Write("\n\nВведите логин:\t");
+            return Console.ReadLine();
+        }
+
+        public bool IsSuscribePremium(string login)
+        {
+            index = 0;
+            while (user[index].Login != login)
+            {
+                index++;
+            }
+
+            return user[index].IsPremium;
+        }
+
+        public void Greetings()
+        {
+            Console.WriteLine("\nДобро пожаловать {0}", user[index].Name);
+        }
+    }
+}
+
